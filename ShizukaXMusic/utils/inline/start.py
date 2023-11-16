a3479297b05d2e3f6e7878d09d45af2f0a2344cf
UNKNOWN-MUSIC <130531308+KwsMusic@users.noreply.github.com>
2023-11-17 02:51:17 +0530

    Update start.py

--- ShizukaXMusic/utils/inline/start.py
+++ ShizukaXMusic/utils/inline/start.py
@@ -67,14 +67,14 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
         buttons.append(
             [
                 InlineKeyboardButton(text=_["S_B_7"], user_id=OWNER),
-                InlineKeyboardButton(text=_["S_B_6"], url=f"https://github.com/KwsMusic/ShizukaXMusic.git"),
+                InlineKeyboardButton(text=_["S_B_6"], url=f"https://github.com/KwsMusic/ShizukaXMusic"),
             ]
         )
     else:
         if GITHUB_REPO:
             buttons.append(
                 [
-                    InlineKeyboardButton(text=_["S_B_6"], url=f"https://github.com/KwsMusic/ShizukaXMusic.git"),
+                    InlineKeyboardButton(text=_["S_B_6"], url=f"https://github.com/KwsMusic/ShizukaXMusic"),
                 ]
             )
         if OWNER:
